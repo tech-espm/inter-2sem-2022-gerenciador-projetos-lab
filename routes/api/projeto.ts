5e673fc434d8d9f3514a0b0e37f104f5a23000a8
gabrielleweyne <100288587+gabrielleweyne@users.noreply.github.com>
2022-06-06 15:54:22 -0300

    commit

---
+++ routes/api/projeto.ts
@@ -0,0 +1,71 @@
+﻿import app = require("teem");
+import Projeto from "../../models/projeto";
+import Usuario = require("../../models/usuario");
+
+class ProjetoApiRoute {
+
+	public static async listar(req: app.Request, res: app.Response) {
+		const u = await Usuario.cookie(req, res, true);
+		if (!u)
+			return;
+
+		res.json(await Projeto.listar());
+	}
+
+	@app.http.post()
+	public static async criar(req: app.Request, res: app.Response) {
+		const u = await Usuario.cookie(req, res, true);
+		if (!u)
+			return;
+
+		const erro = await Projeto.criar(req.body);
+
+		if (erro) {
+			res.status(400).json(erro);
+			return;
+		}
+
+		res.sendStatus(204);
+	}
+
+	@app.http.post()
+	public static async editar(req: app.Request, res: app.Response) {
+		const u = await Usuario.cookie(req, res, true);
+		if (!u)
+			return;
+
+		const erro = await Projeto.editar(req.body);
+
+		if (erro) {
+			res.status(400).json(erro);
+			return;
+		}
+
+		res.sendStatus(204);
+	}
+
+	@app.http.delete()
+	public static async excluir(req: app.Request, res: app.Response) {
+		const u = await Usuario.cookie(req, res, true);
+		if (!u)
+			return;
+
+		const idprojeto = parseInt(req.query["idprojeto"] as string);
+
+		if (isNaN(idprojeto)) {
+			res.status(400).json("Id inválido");
+			return;
+		}
+
+		const erro = await Projeto.excluir(idprojeto);
+
+		if (erro) {
+			res.status(400).json(erro);
+			return;
+		}
+
+		res.sendStatus(204);
+	}
+}
+
+export = ProjetoApiRoute;
--- routes/projeto.ts
+++ routes/projeto.ts
@@ -1,24 +1,26 @@
-﻿import app = require("teem");
-import perfis = require("../models/perfil");
-import Projeto = require("../models/Projeto");
+﻿
+import Projeto from "../models/projeto";
+import Usuario from "../models/usuario";
+import app = require("teem");
+
+
 
 class ProjetoRoute {
 	public static async criar(req: app.Request, res: app.Response) {
-		let u = await Projeto.cookie(req);
+		let u = await Usuario.cookie(req);
 		if (!u || !u.admin)
 			res.redirect(app.root + "/acesso");
 		else
-			res.render("Projeto/editar", {
-				titulo: "Criar Usuário",
+			res.render("projeto/editar", {
+				titulo: "Criar projeto",
 				textoSubmit: "Criar",
-				Projeto: u,
+				usuario: u,
 				item: null,
-				perfis: perfis.lista
 			});
 	}
 
 	public static async editar(req: app.Request, res: app.Response) {
-		let u = await Projeto.cookie(req);
+		let u = await Usuario.cookie(req);
 		if (!u || !u.admin) {
 			res.redirect(app.root + "/acesso");
 		} else {
@@ -27,28 +29,27 @@ class ProjetoRoute {
 			if (isNaN(id) || !(item = await Projeto.obter(id)))
 				res.render("index/nao-encontrado", {
 					layout: "layout-sem-form",
-					Projeto: u
+					usuario: u
 				});
 			else
-				res.render("Projeto/editar", {
-					titulo: "Editar Usuário",
-					Projeto: u,
+				res.render("projeto/editar", {
+					titulo: "Editar projeto",
+					usuario: u,
 					item: item,
-					perfis: perfis.lista
 				});
 		}
 	}
 
 	public static async listar(req: app.Request, res: app.Response) {
-		let u = await Projeto.cookie(req);
+		let u = await Usuario.cookie(req);
 		if (!u || !u.admin)
 			res.redirect(app.root + "/acesso");
 		else
-			res.render("Projeto/listar", {
+			res.render("projeto/listar", {
 				layout: "layout-tabela",
-				titulo: "Gerenciar Usuários",
+				titulo: "Gerenciar projetos",
 				datatables: true,
-				Projeto: u,
+				projeto: u,
 				lista: await Projeto.listar()
 			});
 	}
